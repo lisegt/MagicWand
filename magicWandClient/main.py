cd3baa316eb9bb2228b3873c97e7bfc4d07e5a0e
Lise Gauthier <lise-gauthier@live.fr>
2023-04-06 16:19:18 +0200

    commit before pull

--- magicWandClient/main.py
+++ magicWandClient/main.py
@@ -71,9 +71,9 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("paulort31@laposte.net/lumiere")
-    client.subscribe("paulort31@laposte.net/televiseur")
-    client.subscribe("paulort31@laposte.net/chaine")
+    client.subscribe("paulort31@laposte.net/gestion_lumiere")
+    client.subscribe("paulort31@laposte.net/gestion_televiseur")
+    client.subscribe("paulort31@laposte.net/gestion_chaine")
     client.subscribe("paulort31@laposte.net/roll")
     client.subscribe("paulort31@laposte.net/pitch")
 
@@ -131,75 +131,76 @@ def update_navigateur() :
 
 # réception d'un message sur le topic lumiere
 def on_lumiere_message(client, userdata, msg):
-    acceleration_x = float(msg.payload)
+
+    # acceleration_x = float(msg.payload)
     print(msg.topic+" "+str(msg.payload))
 
-    # action à effectuer
-    global etatLumiere
-    if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
+    # # action à effectuer
+    # global etatLumiere
+    # if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
 
-        if acceleration_x > 0.5 and etatLumiere == False:
-            client.publish(mqtt_topic_gestion_lumiere, "ON")
-            etatLumiere = True
-            print("Lumiere Allumee")
+    #     if acceleration_x > 0.5 and etatLumiere == False:
+    #         client.publish(mqtt_topic_gestion_lumiere, "ON")
+    #         etatLumiere = True
+    #         print("Lumiere Allumee")
 
-        elif acceleration_x > 0.5 and etatLumiere == True:
-            client.publish(mqtt_topic_gestion_lumiere, "OFF")
-            etatLumiere = False
-            print("Lumiere Eteinte")
+    #     elif acceleration_x > 0.5 and etatLumiere == True:
+    #         client.publish(mqtt_topic_gestion_lumiere, "OFF")
+    #         etatLumiere = False
+    #         print("Lumiere Eteinte")
 
-    time.sleep(1)
+    # time.sleep(1)
 
 # réception d'un message sur le topic de televiseur
 def on_televiseur_message(client, userdata, msg):
-    acceleration_y = float(msg.payload)
+    # acceleration_y = float(msg.payload)
     print(msg.topic+" "+str(msg.payload))
 
-    # action à effectuer
-    global etatTele
-    if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
+    # # action à effectuer
+    # global etatTele
+    # if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
 
-        if acceleration_y > 0.5 and etatTele == False:
-            client.publish(mqtt_topic_gestion_televiseur, "ON")
-            etatTele = True
-            print("Tele Allumee")
+    #     if acceleration_y > 0.5 and etatTele == False:
+    #         client.publish(mqtt_topic_gestion_televiseur, "ON")
+    #         etatTele = True
+    #         print("Tele Allumee")
 
-        elif acceleration_y > 0.5 and etatTele == True:
-            client.publish(mqtt_topic_gestion_televiseur, "OFF")
-            etatTele = False
-            print("Tele Eteinte")
+    #     elif acceleration_y > 0.5 and etatTele == True:
+    #         client.publish(mqtt_topic_gestion_televiseur, "OFF")
+    #         etatTele = False
+    #         print("Tele Eteinte")
 
-    time.sleep(1)
+    # time.sleep(1)
     
 # réception d'un message sur le topic de chaine
 def on_chaine_message(client, userdata, msg):
-    acceleration_z = float(msg.payload)
+    # acceleration_z = float(msg.payload)
     print(msg.topic+" "+str(msg.payload))
 
-    global index
+    # global index
 
-    if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
+    # if (roll > -30 and roll < 30) and (pitch > -30 and pitch < 30):
 
-        if etatTele == True:
+    #     if etatTele == True:
 
-            if acceleration_z > 1.5:
-                client.publish(mqtt_topic_gestion_televiseur, "Monter")
-                print("Chaine +1")  
+    #         if acceleration_z > 1.5:
+    #             client.publish(mqtt_topic_gestion_televiseur, "Monter")
+    #             print("Chaine +1")  
                 
-            elif acceleration_z < 0.5:
-                client.publish(mqtt_topic_gestion_televiseur, "Descendre")
-                print("Chaine -1")
-            time.sleep(1)
-        else:
-            print("Veuillez allumer le téléviseur")
+    #         elif acceleration_z < 0.5:
+    #             client.publish(mqtt_topic_gestion_televiseur, "Descendre")
+    #             print("Chaine -1")
+    #         time.sleep(1)
+    #     else:
+    #         print("Veuillez allumer le téléviseur")
 
 # Initilisation du client MQTT
 client = mqtt.Client()
 client.username_pw_set(username="paulort31@laposte.net",password="auzeville31")
 client.on_connect = on_connect
-client.message_callback_add("paulort31@laposte.net/lumiere", on_lumiere_message)
-client.message_callback_add("paulort31@laposte.net/televiseur", on_televiseur_message)
-client.message_callback_add("paulort31@laposte.net/chaine", on_chaine_message)
+client.message_callback_add("paulort31@laposte.net/gestion_lumiere", on_lumiere_message)
+client.message_callback_add("paulort31@laposte.net/gestion_televiseur", on_televiseur_message)
+client.message_callback_add("paulort31@laposte.net/gestion_chaine", on_chaine_message)
 
 client.connect("maqiatto.com", 1883, 60)
 
