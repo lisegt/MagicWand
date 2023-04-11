c512b2398efb0bd1f3d38ae8bfe240f9dd15c3d2
Lise Gauthier <lise-gauthier@live.fr>
2023-04-11 15:58:33 +0200

    lumiere fonctionnelle

--- magicWandLumiere/main.py
+++ magicWandLumiere/main.py
@@ -1,40 +1,57 @@
-from selenium import webdriver
-import socket
+from mqtt import MQTTClient
+from network import WLAN
+import machine
 import time
-import paho.mqtt.client as mqtt
 import pycom
 
 blanc = 0xFFFFFF
 noir = 0x000000
 
+def sub_cb(topic, msg):
+    print("found")
+    print(msg.decode())
+    if msg.decode() == "ON\n":
+        print("test")
+        pycom.rgbled(blanc)
+
+    elif msg.decode() == "OFF\n":
+        pycom.rgbled(noir)
+
+wlan = WLAN(mode=WLAN.STA)
+#wlan.connect("yourwifinetwork", auth=(WLAN.WPA2, "wifipassword"), timeout=5000)
+wlan.connect(ssid="WiFi-Paul", auth=(WLAN.WPA2, "sqoualala"))
+
+while not wlan.isconnected():
+    #machine.idle()
+    print(".",end="")
+    time.sleep(2)
+print("Connected to WiFi\n")
+
+# Déclaration des topics
+mqtt_topic_lumiere = "paulort31@laposte.net/gestion_lumiere"
+
+# Initilisation du client MQTT
+client = MQTTClient("device_id", "loraserver.tetaneutral.net")
+client.connect()
+print("connected")
+client.set_callback(sub_cb)
+client.subscribe(topic="teste")
+print("subbed")
+
 # Désactivation du clignotement de 'base' de la LED 
 pycom.heartbeat(False)
 
-# callback quand le client reçoit une réponse du broker MQTT
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+# # réception d'un message sur le topic lumiere
+# def on_gestion_lumiere_message(client, userdata, msg):
+#     etatLumiere = msg.payload.decode()
+#     print(msg.topic+" "+str(msg.payload))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("paulort31@laposte.net/gestion_lumiere")
+#     
 
-# réception d'un message sur le topic lumiere
-def on_gestion_lumiere_message(client, userdata, msg):
-    etatLumiere = msg.payload.decode()
-    print(msg.topic+" "+str(msg.payload))
 
-    if etatLumiere == "ON":
-        pycom.rgbled(blanc)
 
-    elif etatLumiere == "OFF":
-        pycom.rgbled(noir)
+while True:
+    client.check_msg()
+    time.sleep(1)
 
-# Initilisation du client MQTT
-client = mqtt.Client()
-client.username_pw_set(username="paulort31@laposte.net",password="auzeville31")
-client.on_connect = on_connect
-client.message_callback_add("paulort31@laposte.net/gestion_lumiere", on_gestion_lumiere_message)
-
-client.connect("maqiatto.com", 1883, 60)
-
-client.loop_forever()
+client.disconnect()
