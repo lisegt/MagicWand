12e228e707801f3f483cb87b6ed285dd79f32efe
Tacett <79200344+Tacett@users.noreply.github.com>
2023-04-11 16:12:24 +0200

    update tele

--- magicWandTele/main.py
+++ magicWandTele/main.py
@@ -1,7 +1,6 @@
-from selenium import webdriver
 import socket
 import time
-import paho.mqtt.client as mqtt
+from mqtt import MQTTClient
 import pycom
 
 # Variables pour les couleurs de la LED
@@ -31,8 +30,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("paulort31@laposte.net/gestion_televiseur")
-    #client.subscribe("paulort31@laposte.net/gestion_chaine")
+    client.subscribe("gestion_televiseur")
 
 # réception d'un message sur le topic lumiere
 def on_gestion_televiseur_message(client, userdata, msg):
@@ -72,12 +70,11 @@ def on_gestion_televiseur_message(client, userdata, msg):
     
 
 # Initilisation du client MQTT
-client = mqtt.Client()
-client.username_pw_set(username="paulort31@laposte.net",password="auzeville31")
-client.on_connect = on_connect
-client.message_callback_add("paulort31@laposte.net/gestion_televiseur", on_gestion_televiseur_message)
-#client.message_callback_add("paulort31@laposte.net/gestion_chaine", on_gestion_chaine_message)
-
-client.connect("maqiatto.com", 1883, 60)
+client = MQTTClient("device_id", "loraserver.tetaneutral.net")
+client.connect()
+print("connected")
+client.set_callback(on_gestion_televiseur_message)
+client.subscribe(topic="gestion_televiseur")
+print("subbed")
 
 client.loop_forever()
